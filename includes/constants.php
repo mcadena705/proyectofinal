11f9806b10790392e505638ee495a25ba60c7713
mcadena705 <38122879+mcadena705@users.noreply.github.com>
2018-05-20 00:36:52 -0500

    Update constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 // Database Constants
-define("DB_SERVER", "172.30.13.23");
+define("DB_SERVER", "172.30.86.130");
 define("DB_USER", "user");
 define("DB_PASS", "password");
 define("DB_NAME", "practica");
